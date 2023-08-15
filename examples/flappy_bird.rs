5d506124417c6dd05f754e724ecc9e3c0c4aece2
sgshea <sammys512@yahoo.com>
2023-08-15 12:45:19 -0400

    feat: Modify flappy bird example to have all of a single generation play at once

--- examples/flappy_bird.rs
+++ examples/flappy_bird.rs
@@ -7,30 +7,29 @@ use helper_functions::flappy::*;
 
 async fn eval_genomes(genomes: &mut Vec<Genome>, display: bool) {
     let mut game = Game::new(genomes.len());
+    genomes.sort();
     loop {
         let over = game.update();
         if over {
             break;
         }
         for i in 0..genomes.len() {
-            let (flappy, pipe1, pipe2, x_dist) = game.individual_inputs(i);
-            let inputs = vec![flappy, pipe1, pipe2, x_dist];
+            let (flappy, pipe1, pipe2, x_dist, velocity) = game.individual_inputs(i);
+            let inputs = vec![flappy, pipe1, pipe2, x_dist, velocity];
             let output = genomes[i].feed_forward(inputs.clone());
-            if output[0] > 0.5 {
+            if output[0] > 0.55 {
                 game.flappy[i].jump();
             }
+            if display {
+                let text = format!("1_dist: {:?}", inputs[1]);
+                draw_text(&text, 410.0, 40.0, 30.0, BLACK);
+                let text = format!("2_dist: {:?}", inputs[2]);
+                draw_text(&text, 410.0, 60.0, 30.0, BLACK);
+                let text = format!("outputs: {:?}", output);
+                draw_text(&text, 10.0, 60.0, 30.0, BLACK);
+            }
         }
-        if display {
-            game.draw();
-            // let text = format!("x_dist: {:?}", inputs.last());
-            // draw_text(&text, 10.0, 40.0, 30.0, BLACK);
-            // let text = format!("1_dist: {:?}", inputs[1]);
-            // draw_text(&text, 410.0, 40.0, 30.0, BLACK);
-            // let text = format!("2_dist: {:?}", inputs[2]);
-            // draw_text(&text, 410.0, 60.0, 30.0, BLACK);
-            // let text = format!("outputs: {:?}", output);
-            // draw_text(&text, 10.0, 60.0, 30.0, BLACK);
-        }
+        game.draw();
         next_frame().await
     }
     for i in 0..genomes.len() {
@@ -41,17 +40,19 @@ async fn eval_genomes(genomes: &mut Vec<Genome>, display: bool) {
 #[macroquad::main("BasicShapes")]
 async fn main() {
 
-    let mut population = population::Population::new(250, 4, 1, 0);
+    let mut population = population::Population::new(350, 5, 1, 0);
 
-    for _ in 0..80 {
-        eval_genomes(&mut population.genomes, true).await;
+    for _ in 0..40 {
+        eval_genomes(&mut population.genomes, false).await;
         population.evolve();
         println!("{}", population.get_info());
     }
 
-    // if let Some(ref champion) = population.champion {
-    //     eval_genomes(&mut champion.clone(), true);
-    // }
-    // println!("{}", population.get_info());
+    if let Some(ref champion) = population.champion {
+        loop {
+            eval_genomes(vec![champion.clone()].as_mut(), true).await;
+        }
+    }
+    println!("{}", population.get_info());
 
 }
